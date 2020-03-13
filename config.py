d70fa2202aea9ee8a411608aef280ee1680ae6f3
anjocordero <anjocordero@gmail.com>
2020-03-13 15:42:32 -0700

    add option to search through followed playlists; now removes duplicates

--- config.py
+++ config.py
@@ -3,11 +3,17 @@ username = "121806961"
 
 # Change these variables to filter which songs appear in the playlist.
 BPM = 150
+# 0.0 < X < 1.0
 energy_minimum = 0.5
 danceability_minimum = 0.5
 
 # True if you want to round song BPMs to the tens digit, False if ones digit
-round_to_tens = False
+round_to_tens = True
+
+# Change whether or not to search through followed playlists
+search_playlists = True
 
 # Name of the Spotify playlist to be created
 playlist_name = "Mix Match: " + str(BPM) + " BPM"
+if round_to_tens:
+    playlist_name += "(ish)"
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import spotipy.util as util
 from env import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI,\
     max_track_limit, scope
 from config import username, BPM, playlist_name,\
-    energy_minimum, danceability_minimum, round_to_tens
+    energy_minimum, danceability_minimum, round_to_tens, search_playlists
 
 class Song():
     '''
@@ -15,10 +15,16 @@ class Song():
     '''
 
     def __init__(self, features):
-        self.danceability = features['danceability']
-        self.energy = features['energy']
-        self.tempo = features['tempo']
-        self.id = features['id']
+        self.complete = False
+        if features:
+            self.danceability = features['danceability']
+            self.energy = features['energy']
+            self.tempo = features['tempo']
+            self.id = features['id']
+            self.complete = True
+
+    def is_complete(self):
+        return self.complete
 
 def convert_round():
     if round_to_tens:
@@ -31,27 +37,46 @@ def get_all_saved_tracks(spotify):
     Since the GET request has a limit of 50, this function appends the user's
     entire saved collection to a single object.
     '''
-
     results = spotify.current_user_saved_tracks(limit=max_track_limit)
     saved = results['items']
+
     while (results['next']):
         results = spotify.next(results)
         saved.extend(results['items'])
 
     return saved
 
+def get_all_playlist_tracks(spotify):
+    '''
+    Similar function to get_all_saved_tracks to grab tracks that may not be
+    in user's liked songs.
+    '''
+
+    # Grabs list of playlists instead of single item containing a list.
+    playlists = spotify.current_user_playlists(limit=max_track_limit)['items']
+    playlist_tracks = []
+
+    for playlist in playlists:
+        tracklist = spotify.playlist_tracks(playlist['id'])
+        tracks = tracklist['items']
+        playlist_tracks.extend(tracks)
+
+
+    return playlist_tracks
 
 def process_songs(spotify, saved):
     '''
     Takes the song features returned from the Spotify API and stores the
-    relevant information
+    relevant information. Searches through both liked songs and followed
+    playlists.
     '''
     songs = []
     ids = []
 
     for item in saved:
         id_number = item['track']['id']
-        ids.append(id_number)
+        if id_number:
+            ids.append(id_number)
 
     # audio features returns a list, can take multiple id numbers up to 100
     offset = 0
@@ -63,7 +88,8 @@ def process_songs(spotify, saved):
 
     for feature in features:
         song = Song(feature)
-        songs.append(song)
+        if song.is_complete():
+            songs.append(song)
 
     return songs
 
@@ -78,7 +104,8 @@ def main():
         redirect_uri=SPOTIPY_REDIRECT_URI
     )
 
-#token = util.prompt_for_user_token(username=username,scope=scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
+    # pasted to help paste the command into terminal
+    #token = util.prompt_for_user_token(username=username,scope=scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
 
     if token:
         spotify = spotipy.client.Spotify(auth=token)
@@ -86,23 +113,28 @@ def main():
 
         saved = get_all_saved_tracks(spotify)
 
+        if search_playlists:
+            playlist_songs = get_all_playlist_tracks(spotify)
+            saved.extend(playlist_songs)
+
         songs = process_songs(spotify, saved)
+
+        matches = {song.id for song in songs if 
+            round(song.tempo, round_to_tens) == BPM and
+            song.energy >= energy_minimum and 
+            song.danceability >= danceability_minimum
+        }
 
         playlist = spotify.user_playlist_create(
             user_id, playlist_name, public=False,
             description="Playlist created by Mix Match at " + str(BPM) + " BPM."
         )
-
+        # pasted to help paste the command into terminal
         #playlist = spotify.user_playlist_create(user_id, playlist_name, public=False, description="Playlist created by Mix Match at " + str(BPM) + " BPM.")
-
-        matches = [song.id for song in songs if 
-            round(song.tempo, round_to_tens) == BPM and
-            song.energy >= energy_minimum and 
-            song.danceability >= danceability_minimum
-            ]
 
         spotify.user_playlist_add_tracks(user_id, playlist['id'], matches)
 
 
 if __name__ == "__main__":
     main()
+    print("Complete!")
